09f68c1199534ad358f20e3d2a0d54f98d78bbb0
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-01-09 18:48:57 +0200

    redux store

---
+++ src/redux/transactionSummaryController/transactionSummary-selectors.js
@@ -0,0 +1,5 @@
+export const selectIsLoading = state => state.summary.isLoading;
+
+export const selectError = state => state.summary.error;
+
+export const selectTransactionSummary = state => state.summary.transactionSummary;
