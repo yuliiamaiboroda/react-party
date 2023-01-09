24d7d7c550dc536853c98bd5d319a9d6f645dce9
yuliiamaiboroda <111747754+yuliiamaiboroda@users.noreply.github.com>
2023-01-09 23:00:43 +0000

    Deploying to gh-pages from @ yuliiamaiboroda/react-party@78a9da6cbc9de439a5e410fafd974be0a78bff6d 🚀

---
+++ static/js/664.7bface19.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[664],{6664:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("div",{children:"LoginPage"})}}}]);
+//# sourceMappingURL=664.7bface19.chunk.js.map
--- static/js/974.f6986cdb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[974],{974:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184);function a(){return(0,n.jsx)("div",{children:"RegistrationPage"})}}}]);
-//# sourceMappingURL=974.f6986cdb.chunk.js.map
