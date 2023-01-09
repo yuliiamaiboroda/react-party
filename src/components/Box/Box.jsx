dc1abc8bbfb51fed26f201933214ec8125f8bfcc
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-01-09 14:24:27 +0200

    Add favicon and theme provider

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import {
+  typography,
+  space,
+  color,
+  layout,
+  flexbox,
+  grid,
+  background,
+  border,
+  position,
+  shadow,
+} from 'styled-system';
+
+const Box = styled('div')(
+  typography,
+  space,
+  color,
+  layout,
+  flexbox,
+  grid,
+  background,
+  border,
+  position,
+  shadow
+);
---
+++ src/theme.js
@@ -0,0 +1,42 @@
+export const theme = Object.freeze({
+  borders: {
+    none: 'none',
+    normal: '2px solid black',
+    formCard: '1px solid #c4caec',
+    formField: '2px solid #394ebe',
+  },
+  colors: {
+    dark: '#000',
+    light: '#fff',
+    accent: '#ec3515',
+    disabled: '#999',
+    text: '#000',
+    textLight: '#fff',
+    blue: 'cornflowerblue',
+    primary: '#394EBE',
+    secondary: '#4EBE39',
+    tertiary: '#BE394E',
+  },
+  fontSizes: {
+    xs: 12,
+    s: 16,
+    m: 24,
+    l: 32,
+    xl: 36,
+    xxl: 64,
+  },
+  fontWeights: { normal: 400, bold: 700 },
+  opacities: { none: 0, half: 0.6, full: 1 },
+  radii: { none: 'none', normal: 5, circle: '50%' },
+  shadows: {
+    light: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+    formCard: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+  },
+  sizes: { scaleUp: 1.1, scaleDown: 0.9 },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  transitions: {
+    regularScale: 'scale 250ms linear',
+    regularBgColor: 'background-color 250ms linear',
+  },
+  breakpoints: ['768px', '1280px'],
+});
