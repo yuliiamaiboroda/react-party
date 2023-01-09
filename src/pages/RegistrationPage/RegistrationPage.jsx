21991cdeefbafc7fb88c34442aa87b2b506b087d
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-01-09 11:56:54 +0200

    create folders

---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,5 @@
+export default function RegistrationPage (){
+    return(
+        <div>RegistrationPage</div>
+    )
+}
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import storage from 'redux-persist/lib/storage';
+// import {
+//     persistStore,
+//     persistReducer,
+//     FLUSH,
+//     REHYDRATE,
+//     PAUSE,
+//     PERSIST,
+//     PURGE,
+//     REGISTER,
+//   } from 'redux-persist';
+  
