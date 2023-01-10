b6a4a91de94ce3492c4329a6b13d0c17786a960f
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-01-10 14:19:56 +0200

    Add base form layout

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,35 +1,56 @@
 import { PropTypes } from 'prop-types';
 import { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import Modal from 'components/Modal';
-import ExpenseForm from 'components/ExpenseForm';
-import IncomeForm from 'components/IncomeForm';
 
 export default function ModalAddTransaction({ onClose }) {
-  const [isExpense, setIsExpense] = useState(true);
+  const validationSchema = Yup.object().shape({
+    sum: Yup.number().required(),
+    date: Yup.string().required(),
+    comment: Yup.string(),
+    type: Yup.bool(),
+    colors: Yup.string(),
+  });
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(event.target.elements);
-    onClose();
-  };
   return (
     <Modal onClose={onClose}>
-      <label>
-        Income
-        <input
-          type="checkbox"
-          checked={isExpense}
-          onChange={() => {
-            setIsExpense(!isExpense);
-          }}
-        />
-        Expense
-      </label>
-      {isExpense ? (
-        <ExpenseForm onSubmit={handleSubmit} />
-      ) : (
-        <IncomeForm onSubmit={handleSubmit} />
-      )}
+      <Formik
+        initialValues={{
+          sum: '',
+          date: '',
+          comment: '',
+          type: true,
+          colors: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, action) => {
+          console.log('values: ', values);
+          action.resetForm();
+        }}
+      >
+        {formik => (
+          <Form>
+            <label>
+              Income
+              <Field type="checkbox" name="type" />
+              Expense
+            </label>
+            {formik.values.type && (
+              <Field name="colors" as="select">
+                <option value="">Chose color</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+              </Field>
+            )}
+            <Field type="number" name="sum" />
+            <Field type="date" name="date" />
+            <Field type="text" name="comment" />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </Modal>
   );
 }
