aa74eb9fb918bc812c263000fcf2009bdea763e5
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-01-11 22:19:13 +0200

    remake homeTab and add styles

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,60 +1,67 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
-import { getAllTransactions } from "redux/transactionsController/transactionController-operations";
+import { getAllTransactions , deleteTransaction} from "redux/transactionsController/transactionController-operations";
 import { selectTransictions } from "redux/transactionsController/transactionController-selectors";
 import { getTransactionCategories } from "redux/transactionCategories/transactionCategories-operations";
 import { selectTransactionCategories } from "redux/transactionCategories/transactionCategories-selectors";
+import { Button, Div, H3, Li, P, Ul, Wrapper } from "./HomeTab.styled";
+import { formatDateInStr, formatTransType, switchColor } from "helpers/homeTabHelpers";
 
 export default function HomeTab (){
-const dispatch = useDispatch(); 
-const financeData = useSelector(selectTransictions);
-const transactionCategArr = useSelector(selectTransactionCategories);
+    const dispatch = useDispatch(); 
+    const financeData = useSelector(selectTransictions);
+    const transactionCategArr = useSelector(selectTransactionCategories);
 
-useEffect(()=>{
-    dispatch(getAllTransactions());
-    dispatch(getTransactionCategories())},
-[dispatch]);
+    useEffect(()=>{
+        dispatch(getAllTransactions());
+        dispatch(getTransactionCategories())},
+    [dispatch]);
 
-const sortedArr= [...financeData].sort(
-    (firstTrans, secondTrans)=>Date.parse(secondTrans.transactionDate) - Date.parse(firstTrans.transactionDate)
-    ) 
-    
-const currentTransCateg = categoryId => {
-    const currentTrans = transactionCategArr.find(el=>el.id===categoryId);
-    if (!currentTrans) return "other"
-    return currentTrans.name 
-}
-    return(<>
-        {sortedArr.length === 0
-            ? <h3>No transaction yet</h3>
-            :
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Type</th>
-                    <th>Category</th>
-                    <th>Comment</th>
-                    <th>Sum</th>
-                    <th>Balance</th>
-                </tr>
-            </thead>
-            <tbody>     
-            {sortedArr.map(({transactionDate, type, categoryId, comment, amount, balanceAfter, id})=>(
-                    <tr key={id}>
-                    <td>{transactionDate}</td>
-                    <td>{type}</td>
-                    <td>{currentTransCateg(categoryId)}</td>
-                    <td>{comment}</td>
-                    <td style={
-                        type==="INCOME" ? {color:"#24CCA7"} :
-                         {color: "#FF6596"}
-                    }>{amount}</td>
-                    <td>{balanceAfter}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>}
-        </>
+    const sortedArr= [...financeData].sort(
+        (firstTrans, secondTrans)=>Date.parse(secondTrans.transactionDate) - Date.parse(firstTrans.transactionDate)
+        ); 
+        
+    const currentTransCateg = categoryId => {
+        const currentTrans = transactionCategArr.find(el=>el.id===categoryId);
+        if (!currentTrans) return "other"
+        return currentTrans.name 
+    };
+
+    const hadleDelete = (id) => {
+        dispatch(deleteTransaction(id));
+    }
+
+        return(<>
+            {sortedArr.length === 0
+                ? <H3>No transaction yet</H3>
+                :
+            <Div>
+                <Ul>
+                    <Li>Date</Li>
+                    <Li>Type</Li>
+                    <Li>Category</Li>
+                    <Li>Comment</Li>
+                    <Li>Sum</Li>
+                    <Li>Balance</Li>
+                    <Li>Options</Li>
+                </Ul>
+                {sortedArr.map(({transactionDate, type, categoryId, comment, amount, balanceAfter, id})=>(
+                    <Wrapper key={id}>
+                        <P>{formatDateInStr(transactionDate)}</P>
+                        <P>{formatTransType(type)}</P>
+                        <P>{currentTransCateg(categoryId)}</P>
+                        <P>{comment}</P>
+                        <P style={
+                            {color:switchColor(type)}
+                        }>{amount}</P>
+                        <P>{balanceAfter}</P>
+                        <P>
+                            <Button type="button" onClick={()=>hadleDelete(id)}>delete</Button>
+                        </P>
+                </Wrapper>
+                ))}
+            </Div>
+            }
+            </>
     )
 }
