fceabb0ad0f24ecf4df7e9461a99d817fdd48016
Danil26m <111749790+Danil26m@users.noreply.github.com>
2023-01-12 16:53:54 +0200

    Update Header.styled.js

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import { theme } from 'theme';
 
 export const HeaderLine = styled.header`
   display: flex;
+  background-color: ${theme.colors.lightBg};
   align-items: center;
   justify-content: space-between;
   height: 80px;
@@ -20,18 +22,18 @@ export const Wrapper = styled.div`
 
 export const H2 = styled.h2`
   margin-left: 20px;
-  font-family: ${p => p.theme.fonts.body};
-  font-size: ${p => p.theme.fontSizes.xl}px;
-  font-weight: ${p => p.theme.fontWeights.bold};
-  line-height: ${p => p.theme.lineHeights.heading};
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes.xl}px;
+  font-weight: ${theme.fontWeights.bold};
+  line-height: ${theme.lineHeights.heading};
 `;
 
 export const Name = styled.p`
-  font-family: ${p => p.theme.fonts.heading};
-  font-size: ${p => p.theme.fontSizes.m}px;
-  font-weight: ${p => p.theme.fontWeights.normal};
-  line-height: ${p => p.theme.lineHeights.body};
-  color: ${p => p.theme.colors.muted};
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes.m}px;
+  font-weight: ${theme.fontWeights.normal};
+  line-height: ${theme.lineHeights.body};
+  color: ${theme.colors.muted};
 `;
 
 export const ImgSep = styled.img`
@@ -41,11 +43,11 @@ export const ImgSep = styled.img`
 export const Button = styled.button`
   display: flex;
   align-items: center;
-  font-family: ${p => p.theme.fonts.heading};
-  font-size: ${p => p.theme.fontSizes.m}px;
-  font-weight: ${p => p.theme.fontWeights.normal};
-  line-height: ${p => p.theme.lineHeights.body};
-  color: ${p => p.theme.colors.muted};
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes.m}px;
+  font-weight: ${theme.fontWeights.normal};
+  line-height: ${theme.lineHeights.body};
+  color: ${theme.colors.muted};
   border: none;
   background: none;
   cursor: pointer;
